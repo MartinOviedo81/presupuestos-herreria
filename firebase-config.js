305b865bd6c60bd2500b93afc00cd5d1f9c7a646
MartinOviedo81 <matinoviedo81@gmail.com>
2025-05-05 09:30:37 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,19 +2,19 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js"; // 👈 AGREGADO
+//import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js"; // 👈 AGREGADO
 
 const firebaseConfig = {
   apiKey: "AIzaSyDirRKj8_-oleUdIhDl7LUud-Y9I_T7THQ",
   authDomain: "presupuestos-herreria.firebaseapp.com",
   projectId: "presupuestos-herreria",
-  storageBucket: "presupuestos-herreria.appspot.com", // 👈 NECESARIO PARA STORAGE
+ // storageBucket: "presupuestos-herreria.appspot.com", // 👈 NECESARIO PARA STORAGE
   messagingSenderId: "824709991960",
   appId: "1:824709991960:web:2993a2886f6aa4def3bd6d"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app); // 👈 INSTANCIAR STORAGE
+//const storage = getStorage(app); // 👈 INSTANCIAR STORAGE
 
-export { db, storage }; // 👈 EXPORTAR AMBOS
+export { db }; //, storage }; // 👈 EXPORTAR AMBOS
